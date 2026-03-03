c55a0a3e34141eeb3296ac684e1b38f842eb0829
Zelong Li <87303807+liz435@users.noreply.github.com>
2026-03-02 23:29:47 -0500

    rm deplicate

--- hirarchy_system/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { HierarchyTree } from "@/components/hierarchy-tree"
-import { hierarchyData } from "@/lib/hierarchy-data"
-
-export default function Page() {
-  return <HierarchyTree data={hierarchyData} />
-}
